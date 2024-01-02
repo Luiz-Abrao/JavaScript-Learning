50bfd0813dafbfd963fb2f57d5445c2f4cae99ec
Luiz Henrique Abrão <luizdihenrique@gmail.com>
2024-01-02 17:53:24 -0300

    Atualização

---
+++ Aula 7 - 02.01.24/index.js
@@ -0,0 +1,7 @@
+// const nome = 'João';
+// nome = "Augusto" //Mensagem de erro irá aparecer, pois não é possivel alterar uma constante. Nesse caso teria que utilizar 'let'.
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+console.log(resultado)
