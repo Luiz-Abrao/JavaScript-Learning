ce8c560ced45d54349c352b2ea7025bff272223d
Luiz Henrique Abrão <luizdihenrique@gmail.com>
2024-01-04 16:44:28 -0300

    Curso JS e TS

---
+++ JavaScript Básico/Aula 7 - 02.01.24/index.js
@@ -0,0 +1,15 @@
+// const nome = 'João';
+// nome = "Augusto" //Mensagem de erro irá aparecer, pois não é possivel alterar uma constante. Nesse caso teria que utilizar 'let'.
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(resultadoTriplicado);
+
+console.log(typeof primeiroNumero );
+
+//Alteração teste do codigo para sincronização do servidor local + git
